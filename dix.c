b03d978a1e9d7f9d52beba4550a8cf421241d112
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2025-07-24 17:58:59 +0200

    Pass input file to program as argument

--- dix.c
+++ dix.c
@@ -3,8 +3,13 @@
 #include "io.h"
 #include "lexer.h"
 
-int main() {
-    char* source = read_file("snippets/expression.dix");
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <input.dix>\n", argv[0]);
+        exit(1);
+    }
+
+    char* source = read_file(argv[1]);
     printf("%s\n", source);
 
     TokenArray tokens = lex(source);
